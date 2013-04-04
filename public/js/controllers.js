1610ee6b9eb37c2a8b9dc0d099a968fac6d33248
Lee Collins <javaConductor@yahoo.com>
2013-04-04 18:39:00 -0500

    Init

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,12 +5,49 @@
 function AppCtrl($scope, $http) {
 
 	$scope.title = "Graph O Matic (c)";
-	$scope.viewOptions = {
-		items : [
+
+	$scope.relationships = [{
+		id: 123,
+		name: "Father",
+		itemList: [],
+		: {
+			  baseRelationshipId : 234
+		}
+
+
+	},{}];
+
+	$scope.testItemType = {
+		name : "Person",
+		category: 'People',
+		"titleFunc": function(item ){
+			return item.name;
+		},
+		"imageFunc": function(item ){
+			return item.viewitem.name;
+		},
+		"constraints": [ ]
+	};
+
+	$scope.testViewItems = [{
+		itemId : 123,
+		color: "0000ff",
+		texture: "/textures/ABC.jpg"
+	},{
+
+	}];
+
+	$scope.testItems = [
+
 			{id: 123, position: [50,50], viewItem : {color: '0000FF', texture: '/textures/XYZ.jpg' }, itemType: {category: 'cat123'} },
 			{id: 2123, position: [150,150], viewItem : {color: '00FF00', texture: '/textures/ABC.jpg' }, itemType: {category: 'cat345'} }
 		]
-	};
 
 }
+            /*
 
+             Itemtype.titleFunc: returns a function that can get the title for the items
+             Itemtype.imageFunc: returns a function that produces a link to an image (may be a stock image if none specified)
+
+
+             */
--- public/js/directives.js
+++ public/js/directives.js
@@ -7,9 +7,19 @@ Should look like this:
 
 should turn into:
 
-<svg >
-	<rect x='50' y='50' width='100' height='300' />
-    <text> TITLE </text>
+ <g class="draggable">
+
+	 <rect x="10" y="10" height="100" width="100"
+	    style="stroke:#006600; fill: #00cc00">    	</rect>
+	 <image x="20" y="5" width="80" height="80"
+	    xlink:href="http://jenkov.com/images/layout/top-bar-logo.png" />
+
+	 <text y="100" style="stroke:#00ffff; fill: #00ffff">
+	 <tspan dy="25" y="50" x="15">tspan line 0</tspan>
+	 <tspan dy="25" y="70" x="15">tspan line 1</tspan>
+	 </text>
+
+ </g>
 */
 
 var graphModule = angular.module('graphOmatic.directives', []);

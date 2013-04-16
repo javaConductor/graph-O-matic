1a539f61a72aa228bfa3cd8543fde9c45df38126
Lee Collins <javaConductor@yahoo.com>
2013-04-15 23:11:55 -0500

    Update rest-api.js

--- routes/rest-api.js
+++ routes/rest-api.js
@@ -4,7 +4,13 @@
  * Date: 4/9/13
  * Time: 1:01 AM
  */
-
+ 
+ // get connetion strings
+ 
+ var config = require("/config.js");
+ var usrpswd = config.username + ":" + config.password;
+var db = require("mongojs")(usrpswd+"@"+config.dbHost+"/"+config.dbName, 
+	['Items','Relationship', '']);
 function Api(db)  {
 	var thiz = this;
 	return {

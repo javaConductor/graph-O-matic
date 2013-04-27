9b29acdf13701f04556c80ca28d11e2c0af4add6
Lee Collins <javaConductor@yahoo.com>
2013-04-26 20:19:08 -0500

    little more code - some for Context handling

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,15 +2,48 @@
 
 /* Controllers */
 
-function AppCtrl($scope, $http) {
+function GraphOMaticCtrl($scope, World) {
 
 	$scope.title = "Graph O Matic (c)";
+
+
+	/// get all the views
+	$scope.views = [];
+
+	// get all the categories (item,relationship)
+	$scope.relationshipCategories = [];
+	$scope.itemCategories = [];
+
+	//edit fields
+	$scope.searchText = "";
+
+	// getting back Items
+	$scope.searchResults = [];
+
+	$scope.footerInfo = {
+		poweeredBy: ["nodejs", "mongoDB"]
+	};
+
+	$scope.viewTabList = [
+			{ title:"Some Info page", content:"Info!" }
+		];
+
+
+	$scope.open = function(viewId){
+
+		World.getView(viewId, function(err, viewData){
+			var pane = {viewId:viewData, title:viewData.name, templateUrl:"view.html"}
+			//// add pane to viewTabList after making sure its not there already
+		});
+	}
+
 }
 
 
 function MainViewCtrl($scope, View) {
-
     $scope.title = "Graph O Matic (c)";
+
+	/// get all the views
 }
 
 MainViewCtrl.$inject=['scope','View']
